ff78afaedfcec683a2936ebd445458828d174a64
Michael Hirabayashi <mikehirabayashi@Michaels-MacBook-Air.local>
2016-09-15 23:40:10 -0700

    update Map.rb in ruby folder

--- ruby/ADTs/Map.rb
+++ ruby/ADTs/Map.rb
@@ -7,24 +7,24 @@ class Map
   def assign(key, value)
     included = false
     @map.each_with_index do |pair,i|
-      if pair[0]==key.to_sym
+      if pair[0]==key
         @map[i][1] = value
         included = true
       end
     end
-    @map << [key.to_sym, value] if not included
+    @map << [key, value] if not included
   end
 
   def lookup(key)
     @map.each do |pair|
-      puts "value of #{key} is = #{pair.last}" if pair.first==key.to_sym
-      return pair.last if pair.first.to_sym==key
+      puts "value of #{key} is = #{pair.last}" if pair[0]==key
+      return pair[1] if pair[0]==key
     end
   end
 
   def remove(key)
     @map.each do |pair|
-      @map.delete(pair) if key.to_sym==pair[0]
+      @map.delete(pair) if key==pair[0]
     end
   end
 
